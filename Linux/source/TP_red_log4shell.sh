899d8d78c215458e9498f9b88bc6bfe32446b635
GetSun2EZ <a.darco@hotmail.fr>
2022-05-26 11:58:23 +0200

    Mise en place du menu

---
+++ Linux/source/TP_red_log4shell.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ce script a pour but d'afficher le TP Red_Log4Shell
+#
+# Créateurs		: D'ARCO Adrien, POUDENS Valentin
+# Date de création 	: 22/03/2022
+# Date de mise à jour 	: 26/03/2022
+
+tp_red_log_CLEAR_BANNIERE(){
+/bin/clear
+echo "#################################################################"
+echo "# ______         _______ _                   _           _      #"
+echo "# | ___ \       | | ___ \ |                 | |         | |     #"
+echo "# | |_/ /___  __| | |_/ / |_   _  ___ ______| |     __ _| |__   #"
+echo "# |    // _ \/ _\` | ___ \ | | | |/ _ \______| |    / _\` | '_ \  #"
+echo "# | |\ \  __/ (_| | |_/ / | |_| |  __/      | |___| (_| | |_) | #"
+echo "# \_| \_\___|\__,_\____/|_|\__,_|\___|      \_____/\__,_|_.__/  #"
+echo "#                                                               #"
+echo "#################################################################"
+echo "------------------------------------------------------------------"
+echo " Mise en pratique sur la vulnérabilité Log4Shell en Red Team"
+}
+
+
