2dde2f4a0378e81d86ce6da5bfb27605466cd2db
GetSun2EZ <a.darco@hotmail.fr>
2022-05-26 09:33:08 +0200

    ajout du menu.sh

---
+++ menu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ce script à pour but d'afficher le menu pour accéder aux différents TP
+#
+# Créateurs 		: D'ARCO Adrien, POUDENS Valentin 
+# Date de création 	: 22/03/2022
+# Date de mise à jour 	: 26/05/2022
+
+clear_banniere(){
+	echo "Affichage de la bannière"
+}
+
+
+menu(){
+	clear_banniere
+	echo "Affichage du menu"
+}
+
+
+main(){
+	menu
+}
+
+main
