1f6312836c125e357b2fb15f383f03205867d09a
GetSun2EZ <a.darco@hotmail.fr>
2022-05-26 12:15:12 +0200

    Mise en place d'un menu dans le fichier source

--- Linux/menu.sh
+++ Linux/menu.sh
@@ -2,26 +2,29 @@
 
 # Ce script à pour but d'afficher le menu pour accéder aux différents TP
 #
-# Créateurs 		: D'ARCO Adrien, POUDENS Valentin 
+# Créateurs 		: D'ARCO Adrien, POUDENS Valentin
 # Date de création 	: 22/03/2022
 # Date de mise à jour 	: 26/05/2022
 
 source source/TP_blue_log4shell.sh
-#Fonctions disponibles : 	tp_blue_log_CLEAR_BANNIERE
-#				tp_blue_log_.........
+#Fonctions disponibles : 	tp_blue_log_main
+#				tp_blue_log_CLEAR_BANNIERE
+#				tp_blue_log_..............
 
 source source/TP_blue_zerologon.sh
-#Fonctions disponibles :	tp_blue_zero_CLEAR_BANNIERE
-#				tp_blue_zero_.........
+#Fonctions disponibles :	tp_blue_zero_main
+#				tp_blue_zero_CLEAR_BANNIERE
+#				tp_blue_zero_..............
 
 source source/TP_red_log4shell.sh
-#Fonctions disponibles :	tp_red_log_CLEAR_BANNIERE
-#				tp_red_log_...........
+#Fonctions disponibles :	tp_red_log_main
+#				tp_red_log_CLEAR_BANNIERE
+#				tp_red_log_..............
 
 source source/TP_red_zerologon.sh
-#Fonctions disponibles :	tp_red_log_CLEAR_BANNIERE
-#				tp_red_log_...........
-
+#Fonctions disponibles :	tp_red_log_main
+#				tp_red_log_CLEAR_BANNIERE
+#				tp_red_log_..............
 
 
 clear_banniere(){
@@ -50,23 +53,26 @@ choix(){
     		case $opt in
         		"TP Blue Team sur Log4Shell")
             			echo "C'est parti pour le $opt"
-				tp_blue_log_CLEAR_BANNIERE
+				tp_blue_log_main
 				sleep 5
 				break
             			;;
         		"TP Blue Team sur ZeroLogon")
             			echo "C'est parti pour le $opt"
-				tp_blue_zero_CLEAR_BANNIERE
+				tp_blue_zero_main
+				sleep 5
 				break
 				;;
         		"TP Red Team sur Log4Shell")
             			echo "C'est parti pour le $opt"
-				tp_red_log_CLEAR_BANNIERE
+				tp_red_log_main
+				sleep 5
             			break
 				;;
 			"TP Red Team sur ZeroLogon")
 				echo "C'est parti pour le $opt"
-				tp_red_zero_CLEAR_BANNIERE
+				tp_red_zero_main
+				sleep 5
 				break
 				;;
         		"Quit")
