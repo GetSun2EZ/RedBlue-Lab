359455c07befaae23ba5b2a25c84115d1d1f05de
GetSun2EZ <a.darco@hotmail.fr>
2022-05-26 09:47:11 +0200

    Ajout de tous les scripts TP

---
+++ Linux/source/TP_red_log4shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ce script a pour but d'afficher le TP Red_Log4Shell
+#
+# Créateurs		: D'ARCO Adrien, POUDENS Valentin
+# Date de création 	: 22/03/2022
+# Date de mise à jour 	: 26/03/2022
+
+tp_red_log_CLEAR_BANNIERE(){
+	echo "Affichage de la banniere du TP d'Attaque Log4Shell"
+}
+
+
---
+++ Linux/source/TP_red_zerologon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ce script a pour but d'afficher le TP Red_ZeroLogon
+#
+# Créateurs		: D'ARCO Adrien, POUDENS Valentin
+# Date de création 	: 22/03/2022
+# Date de mise à jour 	: 26/03/2022
+
+tp_red_zero_CLEAR_BANNIERE(){
+	echo "Affichage de la banniere du TP d'Attaque ZeroLogon"
+}
+
+
