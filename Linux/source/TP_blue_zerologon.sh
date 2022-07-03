e8feb865358ac175c2a679010aa9d858aee123a5
GetSun2EZ <44395095+GetSun2EZ@users.noreply.github.com>
2022-07-03 23:00:25 +0200

    Update TP_blue_zerologon.sh

--- Linux/source/TP_blue_zerologon.sh
+++ Linux/source/TP_blue_zerologon.sh
@@ -51,7 +51,7 @@ tp_blue_zero_run_vm(){
 	echo -e "\n\n"
 }
 
-tp_red_zero_etape_1(){
+tp_blue_zero_etape_1(){
 	echo "------------------------------------------------------------------"
 	echo " Etape 1 - Explication de la vulnérabilité"
 	echo "------------------------------------------------------------------"
