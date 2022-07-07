e9c074d5c0e270fe00d657e241fabc57a82bd525
GetSun2EZ <44395095+GetSun2EZ@users.noreply.github.com>
2022-07-08 00:09:35 +0200

    Update TP_blue_log4shell.sh

--- Linux/source/TP_blue_log4shell.sh
+++ Linux/source/TP_blue_log4shell.sh
@@ -41,11 +41,11 @@ tp_blue_log_run_vm(){
 	if [ ! -d ../VM/vm_blue_log ] && [ ! -e ../VM/vm_blue_log.zip ]
 	then
 	  wget -q --show-progress http://62.212.90.183:9090/vm_blue_log.zip -O ../VM/vm_blue_log.zip
-	  unzip ../VM/vm_blue_log.zip
+	  unzip ../VM/vm_blue_log.zip -d ../VM/
 	fi
 	if [ ! -d ../VM/vm_blue_log ] && [ -e ../VM/vm_blue_log.zip ]
 	then
-	  unzip ../VM/vm_blue_log.zip
+	  unzip ../VM/vm_blue_log.zip -d ../VM/
 	fi
 	
 	echo "[~] Lancement de la VM vulnérable"
